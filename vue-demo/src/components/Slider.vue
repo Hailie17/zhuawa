83099d7bef9b9fe099cb6fc97d268ab51ce7abd0
Hailie17 <192151452@qq.com>
2024-02-02 23:35:14 +0800

    创建slider组件

---
+++ vue-demo/src/components/Slider.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="slider">
+    <span v-for="(tab, index) in tabs" :key="tab + index" :class="{ active: activeTab === index }" class="tab-item" @click="'changeActiveTab(index)'">{{ tab }}</span>
+  </div>
+</template>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+export default class Slider extends Vue {
+  tabs: string[] = ['推荐', '推荐','推荐','推荐','推荐','推荐','推荐','推荐'];
+  activeTab = 0;
+
+  changeActiveTab(index: number) {
+    this.activeTab = index
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
