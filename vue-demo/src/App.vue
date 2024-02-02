2f3e5f1675e7449c6440525424412d1f7e92ee05
Hailie17 <192151452@qq.com>
2024-02-02 23:47:05 +0800

    媒体查询

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
 
 <style lang="less">
@@ -15,16 +13,15 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+html {
+  width: 100%;
+  height: 100%;
+  font-size: 10px;
+}
+// 媒体查询
+@media screen and (max-width: 338px) {
+  html {
+    font-size: 8px !important;
   }
 }
 </style>
