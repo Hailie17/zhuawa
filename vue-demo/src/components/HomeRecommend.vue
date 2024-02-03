11937788f975aa164aa2622757d3e89f2bafec13
Hailie17 <192151452@qq.com>
2024-02-04 02:14:40 +0800

    创建推荐列表组件

---
+++ vue-demo/src/components/HomeRecommend.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home-recommend">
+   <h3>推荐</h3>
+   <div class="recommend-container"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {
+    
+  },
+})
+export default class HomeRecommend extends Vue {
+ 
+}
+</script>
