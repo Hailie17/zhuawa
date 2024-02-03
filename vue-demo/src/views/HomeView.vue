125d7bc5b93265b49eb2656616206daae12c9557
Hailie17 <192151452@qq.com>
2024-02-03 23:54:38 +0800

    1

--- vue-demo/src/views/HomeView.vue
+++ vue-demo/src/views/HomeView.vue
@@ -6,6 +6,8 @@
 </template>
 
 <script lang="ts">
+
+
 import { Options, Vue } from 'vue-class-component';
 import Slider from '@/components/Slider.vue'; // @ is an alias to /src
 import Banner from '@/components/Banner.vue';
