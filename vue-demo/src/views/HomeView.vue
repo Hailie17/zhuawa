8808332cbbec59be43106e4a5040ac1228c70aa1
Hailie17 <192151452@qq.com>
2024-02-03 23:25:25 +0800

    readonly 只读属性

--- vue-demo/src/views/HomeView.vue
+++ vue-demo/src/views/HomeView.vue
@@ -16,5 +16,16 @@ import Banner from '@/components/Banner.vue';
     Banner
   },
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+  readonly BANNER_LIST = [ // 只读属性，用常量命名
+    "https://img.tukuppt.com//ad_preview/00/11/13/5c9940242bf11.jpg!/fw/780",
+    "https://img.tukuppt.com//ad_preview/00/03/56/5c98abb074bdd.jpg!/fw/780",
+    "https://img.tukuppt.com//ad_preview/00/10/15/5c992911709f7.jpg!/fw/780",
+    "https://img.tukuppt.com//ad_preview/00/19/70/5c9a0504de105.jpg!/fw/780",
+    "http://pic.616pic.com/bg_w1180/00/14/61/bjDQ7hTsKA.jpg!/fw/1120",
+    "https://img.tukuppt.com//ad_preview/00/10/15/5c992911709f7.jpg!/fw/780",
+    "https://img.tukuppt.com//ad_preview/00/19/70/5c9a0504de105.jpg!/fw/780",
+    "http://pic.616pic.com/bg_w1180/00/14/61/bjDQ7hTsKA.jpg!/fw/1120"
+  ];
+}
 </script>
