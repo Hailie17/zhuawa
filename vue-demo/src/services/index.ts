b375193d85797f43524eb241c7509d8bc5fc46f6
Hailie17 <192151452@qq.com>
2024-02-04 22:01:22 +0800

    推荐列表接口

--- vue-demo/src/services/index.ts
+++ vue-demo/src/services/index.ts
@@ -4,4 +4,9 @@ export default class Services {
   static init () {  // 定义了static静态方法，就不会走构造函数，而是直接执行静态方法
     axios.defaults.baseURL = 'https://www.fastmock.site/mock/091ed7b7b0b6115a7da81c9b77b7725d/index'
   }
+
+  // 推荐列表
+  static getRecommendList () {
+    return axios.get('/homeRecommendList').then(res => res.data)
+  }
 }
