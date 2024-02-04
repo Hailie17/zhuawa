060dc2711e5439ad814bed742a933d599309d182
Hailie17 <192151452@qq.com>
2024-02-04 21:58:39 +0800

    类里定义了static静态方法，就不会走构造函数，而是直接执行静态方法

--- vue-demo/src/services/index.ts
+++ vue-demo/src/services/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default class Services {
-  static init () {
+  static init () {  // 定义了static静态方法，就不会走构造函数，而是直接执行静态方法
     axios.defaults.baseURL = 'https://www.fastmock.site/mock/091ed7b7b0b6115a7da81c9b77b7725d/index'
   }
 }
