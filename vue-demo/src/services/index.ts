10465c55ac40475aa4d886dd22c538a2e95c1348
Hailie17 <192151452@qq.com>
2024-02-06 23:16:53 +0800

    currentlist

--- vue-demo/src/services/index.ts
+++ vue-demo/src/services/index.ts
@@ -1,15 +1,21 @@
 import axios from "axios";
-import { HomeRecommendListItem } from "./types";
+import { HomeRecommendListItem, DetailInfo } from "./types";
 
 export default class Services {
   static init () {  // 定义了static静态方法，就不会走构造函数，而是直接执行静态方法
     axios.defaults.baseURL = 'https://www.fastmock.site/mock/091ed7b7b0b6115a7da81c9b77b7725d/index'
   }
 
-  // 推荐列表
-  static getRecommendList () {
+  // 首页推荐列表
+  static async getRecommendList () {
     this.init()
     // 请求回来的结果 res.data 是 HomeRecommendListItem[] 类型的
     return axios.get<HomeRecommendListItem[]>('/homeRecommendList').then(res => res.data)
+  }
+
+  // 列表
+  static async getList () {
+    this.init()
+    return axios.get<DetailInfo[]>('/list').then(res => res.data)
   }
 }
--- vue-demo/src/views/List.vue
+++ vue-demo/src/views/List.vue
@@ -1,5 +1,56 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="list-page">
+    <div class="input-container">
+      <input type="text" v-model="search" class="search" placeholder="请输入关键字搜索">
+    </div>
+    <div class="list-container">
+      <div class="list-item" v-for="item in currentList" :key="item.id" @click="toDetail(item.id)">
+        <div class="left-section"></div>
+        <div class="right-section"></div>
+      </div>
+    </div>
   </div>
 </template>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Services from '@/services'
+import { DetailInfo } from '@/services/types'
+
+@Options({
+  components: {
+    
+  },
+})
+export default class List extends Vue {
+  search = '';
+  textList: DetailInfo[] = []
+
+  get currentList () {
+    // 关键字搜索后实际要展示的列表
+    return this.textList.filter(item  => item.title.indexOf(this.search) > -1 )
+  }
+  toDetail(id: number) {
+    // TODO 跳转到详情页
+
+  }
+
+  async created() {
+    this.textList = await Services.getList()
+  }
+}
+</script>
+<style lang="less" scoped>
+.list-container {
+  .search {
+    outline-style: none;
+    border: 1px solid #ccc;
+    border-radius: 0.3rem;
+
+    padding: 1rem;
+    width: 60%;
+    margin: 1rem auto;
+    font-size: 1.6rem;
+    text-align: center;
+  }
+}
+</style> 
