187e9b9d22c92402e53468cd38b1d1cbe59f1b8e
Hailie17 <192151452@qq.com>
2024-02-08 01:26:04 +0800

    vuex存数据

--- vue-demo/src/store/index.ts
+++ vue-demo/src/store/index.ts
@@ -2,10 +2,14 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    detailInfos: []
   },
   getters: {
   },
   mutations: {
+    setDetailInfo(state, payload) {
+      state.detailInfos = payload
+    }
   },
   actions: {
   },
