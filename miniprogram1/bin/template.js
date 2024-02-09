f5e5fa4675f8d1a7df20a3fad7f27b708c9ec141
Hailie17 <192151452@qq.com>
2024-02-09 18:17:01 +0800

    创建页面的模板

--- miniprogram1/bin/template.js
+++ miniprogram1/bin/template.js
@@ -1,1 +1,52 @@
-const PageTemplate = (pageName) => ``
+const PageTemplate = (pageName) => `<template>
+	<view class="${pageName}">
+	</view>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+
+	export default Vue.extend({
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	});
+</script>
+
+<style>
+</style>
+`
+
+const ComponentTemplate = (componentName) => `<template>
+	<view class="${pageName}">
+	</view>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+
+	export default Vue.extend({
+		data() {
+			return {
+				
+			}
+		},
+		onAttach() {
+
+		},
+		methods: {
+
+		}
+	});
+</script>
+
+<style>
+</style>`
