0686feb1325782643586aa04b718a8f5f96340a9
Hailie17 <192151452@qq.com>
2024-02-10 23:47:00 +0800

    网络错误提示

--- miniprogram1/src/lib/request.ts
+++ miniprogram1/src/lib/request.ts
@@ -2,17 +2,53 @@
  * 封装请求
  */
 
+
 type RequestMethod = "GET" | "POST" | "PUT" | "OPTIONS" | "HEAD" | "DELETE" | "TRACE" | "CONNECT"
 
 const request = async(url: string, params?: object, method: RequestMethod = 'GET', header: Object={}) => {
-	return await new Promise(() => {
+	return await new Promise((resolve, reject) => {
 		uni.request({
 			url,
 			method,
 			header,
 			data: {
 				...params
+			},
+			success: (res) => {
+				if(res.statusCode !== 200) {
+					reject(res.data)
+				} else {
+					resolve(res.data)
+				}
+			},
+			fail: (e) => {
+				reject(e)
 			}
 		})
+	}).catch(e => {
+		const errorMsg = `${method} ${url} failed: ${e.status} ${e.message}`
+		console.error(new Error(errorMsg))
+		commonErrorHandler(e)
 	})
+}
+
+/**
+ * 错误处理
+ */
+const commonErrorHandler = e => {
+	let msg = ''
+	if(e.status > 499) {
+		// 5xx错误一般是服务端错误，所以单独处理
+		msg = '服务器错误'
+	} else if (e.errMsg === 'request:fail' || e.errMsg === 'request:fail timeout'){
+		// 可能是断网或者弱网
+		msg = '无法连接至服务器'
+	}
+	if(msg) {
+		uni.showToast({
+			title: msg,
+			icon: 'none',
+			duration: 4000
+		})
+	}
 }
