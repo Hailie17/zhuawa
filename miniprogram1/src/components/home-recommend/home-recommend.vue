bf6667a66b4bc787630349e350399b3b834a487d
Hailie17 <192151452@qq.com>
2024-02-12 22:13:27 +0800

    跳转页面

--- miniprogram1/src/components/home-recommend/home-recommend.vue
+++ miniprogram1/src/components/home-recommend/home-recommend.vue
@@ -4,7 +4,7 @@
 		<view class="recommend-container">
 			<view class="recommend-item" v-for="(item, index) in list" :key='{item}' :class="{'recommend-hover': activeIndex === index}" @click="onClick(index)">
 				<img :src="item.image" alt="" />
-				<view class="">
+				<view class="text">
 					{{ item.text }}
 				</view>
 			</view>
@@ -24,15 +24,48 @@ export default class HomeRecommend extends Vue {
 	
 	onClick(index: number) {
 		this.activeIndex = index
+		// 跳转
+		setTimeout(() => {
+			uni.navigateTo({
+				url: '/pages/list/index'
+			})
+		}, 200)
 	}
 	
 	async created() {
 		this.list = await Services.getHomeRecommendList()
-		console.log(this.list,999);
 	}
 }
 </script>
 
 <style>
-
+.recommend-hover {
+	transform: translate3d(2upx, 2upx, 0);
+}
+h3 {
+	font-size: 32upx;
+	font-weight: 600;
+}
+.recommend-container {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+}
+.recommend-item {
+	width: 212upx;
+	height: 212upx;
+	margin-bottom: 60upx;
+	background: gray;
+	border-radius: 10upx;
+	box-shadow: 0 20upx 40upx -8upx rgba(106, 182, 252, 0.5);
+}
+img {
+	border-radius: 10upx;
+	height: 100%;
+	width: 100%;
+}
+.text {
+	font-size: 24upx;
+	text-align: center;
+}
 </style>
---
+++ miniprogram1/src/pages/list/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class List extends Vue {
+	
+}
+</script>
+
+<style>
+
+</style>
